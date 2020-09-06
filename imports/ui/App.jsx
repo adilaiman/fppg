708ace5441339a08b148b731ca2c78ed08019a6a
Adi Laiman <44214543+adilaiman@users.noreply.github.com>
2020-09-06 22:52:51 +0100

    Load JSON into players, fixtures and team collections

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -3,7 +3,5 @@ import React from 'react';
 export const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
   </div>
 );
--- server/main.js
+++ server/main.js
@@ -1,4 +1,38 @@
 import { Meteor } from 'meteor/meteor';
+import { HTTP } from 'meteor/http';
+
+import Fixtures from '/imports/api/fixtures';
+import Players from '/imports/api/players';
+import Teams from '/imports/api/teams';
+
+function insertIntoCollections(data) {
+    const keys = Object.keys(data);
+    keys.splice(0,1);
+
+    keys.forEach(key => {
+        if (key === 'fixtures') {
+        data[key].forEach(entry => {
+            Fixtures.insert(entry);
+        });
+        } else if (key === 'players') {
+        data[key].forEach(entry => {
+            Players.insert(entry);
+        });
+        } else if (key === 'teams') {
+        data[key].forEach(entry => {
+            Teams.insert(entry);
+        });
+        } else {
+        return;
+        }
+    });
+}
+
+const url = 'https://gist.githubusercontent.com/liamjdouglas/bb40ee8721f1a9313c22c6ea0851a105/raw/6b6fc89d55ebe4d9b05c1469349af33651d7e7f1/Player.json';
 
 Meteor.startup(() => {
+    if (Players.find({}).fetch().length === 0) {
+        const myJSON = JSON.parse(HTTP.call('GET', url).content);
+        insertIntoCollections(myJSON);
+    }
 });
